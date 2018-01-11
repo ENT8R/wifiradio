1912fe29e6f38d425c06dac6f1f91d79c5fa9271
Massimiliano Cannarozzo <maxcanna@gmail.com>
2018-01-11 12:24:41 +0100

    Fix indentation
    Use four spaces

--- index.js
+++ index.js
@@ -6,131 +6,131 @@ let pin;
 
 //GET a session ID
 function getSessionID() {
-  return new Promise(function(callback) {
-    request({
-      url: `http://${ip}/fsapi/CREATE_SESSION`,
-      qs: {
-        pin,
-      }
-    }, function(error, response, body) {
-      parseXML(body, function(err, parseResult) {
-        callback(parseResult.fsapiResponse.sessionId[0]);
-      });
+    return new Promise(function (callback) {
+        request({
+            url: `http://${ip}/fsapi/CREATE_SESSION`,
+            qs: {
+                pin,
+            },
+        }, function (error, response, body) {
+            parseXML(body, function (err, parseResult) {
+                callback(parseResult.fsapiResponse.sessionId[0]);
+            });
+        });
     });
-  });
 };
 
 //Make a request with a given operation and a possible value
 function makeRequest(operation, value = '') {
-  return new Promise(function(callback) {
-    getSessionID().then(function(sid) {
-      request({
-        url: `http://${ip}/fsapi/${operation}`,
-        qs: {
-          pin,
-          sid,
-          value,
-        }
-      }, function(error, response, body) {
-        parseXML(body, function(err, result) {
-          callback(result);
+    return new Promise(function (callback) {
+        getSessionID().then(function (sid) {
+            request({
+                url: `http://${ip}/fsapi/${operation}`,
+                qs: {
+                    pin,
+                    sid,
+                    value,
+                },
+            }, function (error, response, body) {
+                parseXML(body, function (err, result) {
+                    callback(result);
+                });
+            });
         });
-      });
     });
-  });
 };
 
 //SET Power
 function setPower(state) {
-  makeRequest('SET/netRemote.sys.power', state.toString());
+    makeRequest('SET/netRemote.sys.power', state.toString());
 };
 
 //GET Power
 function getPower(callback) {
-  makeRequest('GET/netRemote.sys.power').then(function(result) {
-    callback(parseInt(result.fsapiResponse.value[0].u8[0]));
-  });
+    makeRequest('GET/netRemote.sys.power').then(function (result) {
+        callback(parseInt(result.fsapiResponse.value[0].u8[0]));
+    });
 };
 
 //GET the first line of the display
 function getName(callback) {
-  makeRequest('GET/netRemote.play.info.name').then(function(result) {
-    callback(result.fsapiResponse.value[0].c8_array[0]);
-  });
+    makeRequest('GET/netRemote.play.info.name').then(function (result) {
+        callback(result.fsapiResponse.value[0].c8_array[0]);
+    });
 };
 
 //GET the second line of the display
 function getText(callback) {
-  makeRequest('GET/netRemote.play.info.text').then(function(result) {
-    callback(result.fsapiResponse.value[0].c8_array[0]);
-  });
+    makeRequest('GET/netRemote.play.info.text').then(function (result) {
+        callback(result.fsapiResponse.value[0].c8_array[0]);
+    });
 };
 
 //SET mute state
 function setMute(state) {
-  makeRequest('SET/netRemote.sys.audio.mute', state.toString());
+    makeRequest('SET/netRemote.sys.audio.mute', state.toString());
 };
 
 //GET mute state
 function getMute(callback) {
-  makeRequest('GET/netRemote.sys.audio.mute').then(function(result) {
-    callback(parseInt(result.fsapiResponse.value[0].u8[0]));
-  });
+    makeRequest('GET/netRemote.sys.audio.mute').then(function (result) {
+        callback(parseInt(result.fsapiResponse.value[0].u8[0]));
+    });
 };
 
 //SET current mode
 function setMode(mode) {
-  makeRequest('SET/netRemote.sys.mode', mode.toString());
+    makeRequest('SET/netRemote.sys.mode', mode.toString());
 };
 
 //GET current mode
 function getMode(callback) {
-  makeRequest('GET/netRemote.sys.mode').then(function(result) {
-    callback(parseInt(result.fsapiResponse.value[0].u32[0]));
-  });
+    makeRequest('GET/netRemote.sys.mode').then(function (result) {
+        callback(parseInt(result.fsapiResponse.value[0].u32[0]));
+    });
 };
 
 //LIST available modes; returns a JSON array of all available modes
 //TODO: this method does not work at the moment. Needs some research
 function listModes(callback) {
-  makeRequest('LIST_GET_NEXT/netRemote.sys.caps.validModes/-1').then(function(result) {
-    callback(JSON.stringify(result.fsapiResponse.item[0].field));
-  });
+    makeRequest('LIST_GET_NEXT/netRemote.sys.caps.validModes/-1').then(function (result) {
+        callback(JSON.stringify(result.fsapiResponse.item[0].field));
+    });
 }
 
 //SET the volume; accepts all values from 1-20
 function setVolume(value) {
-  makeRequest('SET/netRemote.sys.audio.volume', value.toString());
+    makeRequest('SET/netRemote.sys.audio.volume', value.toString());
 };
 
 //GET the volume
 function getVolume(callback) {
-  makeRequest('GET/netRemote.sys.audio.volume').then(function(result) {
-    callback(parseInt(result.fsapiResponse.value[0].u8[0]));
-  });
+    makeRequest('GET/netRemote.sys.audio.volume').then(function (result) {
+        callback(parseInt(result.fsapiResponse.value[0].u8[0]));
+    });
 };
 
 //And finally export the module
-module.exports = function(ipAdress, radioPin) {
-  if (!ipAdress) throw new Error('No IP adress specified');
-  if (!radioPin) throw new Error('No PIN specified');
+module.exports = function (ipAdress, radioPin) {
+    if (!ipAdress) throw new Error('No IP adress specified');
+    if (!radioPin) throw new Error('No PIN specified');
 
-  ip = ipAdress;
-  pin = radioPin;
+    ip = ipAdress;
+    pin = radioPin;
 
-  this.getPower = getPower;
-  this.setPower = setPower;
+    this.getPower = getPower;
+    this.setPower = setPower;
 
-  this.getName = getName;
-  this.getText = getText;
+    this.getName = getName;
+    this.getText = getText;
 
-  this.setMute = setMute;
-  this.getMute = getMute;
+    this.setMute = setMute;
+    this.getMute = getMute;
 
-  this.setMode = setMode;
-  this.getMode = getMode;
+    this.setMode = setMode;
+    this.getMode = getMode;
 
-  this.setVolume = setVolume;
-  this.getVolume = getVolume;
+    this.setVolume = setVolume;
+    this.getVolume = getVolume;
 
 };
