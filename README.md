7b03c7e562ee39336c0bb5f62695d84de1b26943
Massimiliano Cannarozzo <maxcanna@gmail.com>
2018-01-12 21:00:47 +0100

    Use promises (#2)
    * Use string interpolation
    
    * Add default value
    
    * Use query params
    
    * Fix indentation
    
    Use four spaces
    
    * Use arrow function
    
    * Add error handling
    
    * Default value is no longer necessary
    
    * Return promises
    
    * Fix style
    
    * Update README.md

--- README.md
+++ README.md
@@ -7,20 +7,19 @@
 NodeJS module for controlling WiFi-radios
 
 ## Installation
-		npm install wifiradio --save
+    npm install wifiradio --save
 
 ## Usage
 ```js
 const wifiradio = require('wifiradio');
 
-const ip = "192.168.178.27"; //Change this to the ip adress of your radio
-const pin = "1234"; //This is the default PIN for the radio. (Works in most cases)
+const ip = '192.168.178.27'; //Change this to the ip adress of your radio
+const pin = '1234'; //This is the default PIN for the radio. (Works in most cases)
 
 const radio = new wifiradio(ip, pin);
 
-radio.getPower(function(val){
-	console.log(val);
-});
+radio.getPower()
+    .then(console.log);
 ```
 
 ## Features
@@ -35,9 +34,8 @@ radio.setPower(1);
 radio.setPower(0);
 
 //Get whether the radio is on or off (returns 0 or 1)
-radio.getPower(function(val){
-	console.log(val);
-});
+radio.getPower()
+    .then(console.log);
 ```
 
 ### Mute
@@ -48,12 +46,10 @@ radio.setMute(1);
 
 //Mute off
 radio.setMute(0);
-});
 
 //Get whether the radio is mute or not (returns 0 or 1)
-radio.getMute(function(val){
-	console.log(val);
-});
+radio.getMute()
+    .then(console.log);
 ```
 
 ### Volume
@@ -63,9 +59,8 @@ radio.getMute(function(val){
 radio.setVolume(10);
 
 //Get the current volume (returns a value from 1-20)
-radio.getVolume(function(val){
-	console.log(val);
-});
+radio.getVolume()
+    .then(console.log);
 ```
 
 ### Modes
@@ -75,23 +70,20 @@ radio.getVolume(function(val){
 radio.setMode(2);
 
 //Get the current playing mode
-radio.getMode(function(val){
-	console.log(val);
-});
+radio.getMode()
+    .then(console.log);
 ```
 
 ### Display
 
 ```js
 //Get the first line of the display
-radio.getName(function(val){
-	console.log(val);
-});
+radio.getName()
+    .then(console.log);
 
 //Get the second line of the display
-radio.getText(function(val){
-	console.log(val);
-});
+radio.getText()
+    .then(console.log);
 ```
 
 ## Contributing
@@ -99,4 +91,4 @@ radio.getText(function(val){
 There are many more requests that could be done by this module. If you think that something is missing just open an issue for that or make a pull request. If you need some help, you can have a look [here](https://github.com/flammy/fsapi/blob/master/FSAPI.md) for some further requests.
 
 ## License
-[GPL-3.0](https://github.com/ENT8R/HTMLEmails/blob/master/LICENSE)
+[GPL-3.0](https://github.com/ENT8R/wifiradio/blob/master/LICENSE)
--- index.js
+++ index.js
@@ -6,119 +6,140 @@ let pin;
 
 //GET a session ID
 function getSessionID() {
-  return new Promise(function(callback) {
-    request('http://' + ip + '/fsapi/CREATE_SESSION?pin=' + pin, function(error, response, body) {
-      parseXML(body, function(err, parseResult) {
-        callback(parseResult.fsapiResponse.sessionId[0]);
-      });
+    return new Promise((resolve, reject) => {
+        request({
+            url: `http://${ip}/fsapi/CREATE_SESSION`,
+            qs: {
+                pin,
+            },
+        }, (error, response, body) => {
+            if (error || response.statusCode !== 200) {
+                return reject(error);
+            }
+            parseXML(body, (err, parseResult) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(parseResult.fsapiResponse.sessionId[0]);
+            });
+        });
     });
-  });
 };
 
 //Make a request with a given operation and a possible value
 function makeRequest(operation, value) {
-  return new Promise(function(callback) {
-    getSessionID().then(function(sessionId) {
-      request('http://' + ip + '/fsapi/' + operation + '?pin=' + pin + '&sid=' + sessionId + '&value=' + value, function(error, response, body) {
-        parseXML(body, function(err, result) {
-          callback(result);
+    return new Promise((resolve, reject) => {
+        getSessionID().then(sid => {
+            request({
+                url: `http://${ip}/fsapi/${operation}`,
+                qs: {
+                    pin,
+                    sid,
+                    value,
+                },
+            }, (error, response, body) => {
+                if (error || response.statusCode !== 200) {
+                    return reject(error);
+                }
+                parseXML(body, (err, result) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    resolve(result);
+                });
+            });
         });
-      });
     });
-  });
 };
 
 //SET Power
 function setPower(state) {
-  makeRequest('SET/netRemote.sys.power', state.toString());
+    return makeRequest('SET/netRemote.sys.power', state.toString());
 };
 
 //GET Power
-function getPower(callback) {
-  makeRequest('GET/netRemote.sys.power', '').then(function(result) {
-    callback(parseInt(result.fsapiResponse.value[0].u8[0]));
-  });
+function getPower() {
+    return makeRequest('GET/netRemote.sys.power')
+        .then(result => parseInt(result.fsapiResponse.value[0].u8[0]));
 };
 
 //GET the first line of the display
-function getName(callback) {
-  makeRequest('GET/netRemote.play.info.name', '').then(function(result) {
-    callback(result.fsapiResponse.value[0].c8_array[0]);
-  });
+function getName() {
+    return makeRequest('GET/netRemote.play.info.name')
+        .then(result => result.fsapiResponse.value[0].c8_array[0]);
 };
 
 //GET the second line of the display
-function getText(callback) {
-  makeRequest('GET/netRemote.play.info.text', '').then(function(result) {
-    callback(result.fsapiResponse.value[0].c8_array[0]);
-  });
+function getText() {
+    return makeRequest('GET/netRemote.play.info.text')
+        .then(result => result.fsapiResponse.value[0].c8_array[0]);
 };
 
 //SET mute state
 function setMute(state) {
-  makeRequest('SET/netRemote.sys.audio.mute', state.toString());
+    return makeRequest('SET/netRemote.sys.audio.mute', state.toString());
 };
 
 //GET mute state
-function getMute(callback) {
-  makeRequest('GET/netRemote.sys.audio.mute', '').then(function(result) {
-    callback(parseInt(result.fsapiResponse.value[0].u8[0]));
-  });
+function getMute() {
+    return makeRequest('GET/netRemote.sys.audio.mute')
+        .then(result => parseInt(result.fsapiResponse.value[0].u8[0]));
 };
 
 //SET current mode
 function setMode(mode) {
-  makeRequest('SET/netRemote.sys.mode', mode.toString());
+    return makeRequest('SET/netRemote.sys.mode', mode.toString());
 };
 
 //GET current mode
-function getMode(callback) {
-  makeRequest('GET/netRemote.sys.mode', '').then(function(result) {
-    callback(parseInt(result.fsapiResponse.value[0].u32[0]));
-  });
+function getMode() {
+    return makeRequest('GET/netRemote.sys.mode')
+        .then(result => parseInt(result.fsapiResponse.value[0].u32[0]));
 };
 
 //LIST available modes; returns a JSON array of all available modes
 //TODO: this method does not work at the moment. Needs some research
-function listModes(callback) {
-  makeRequest('LIST_GET_NEXT/netRemote.sys.caps.validModes/-1', '').then(function(result) {
-    callback(JSON.stringify(result.fsapiResponse.item[0].field));
-  });
+function listModes() {
+    return makeRequest('LIST_GET_NEXT/netRemote.sys.caps.validModes/-1')
+        .then(result => JSON.stringify(result.fsapiResponse.item[0].field));
 }
 
 //SET the volume; accepts all values from 1-20
 function setVolume(value) {
-  makeRequest('SET/netRemote.sys.audio.volume', value.toString());
+    return makeRequest('SET/netRemote.sys.audio.volume', value.toString());
 };
 
 //GET the volume
-function getVolume(callback) {
-  makeRequest('GET/netRemote.sys.audio.volume', '').then(function(result) {
-    callback(parseInt(result.fsapiResponse.value[0].u8[0]));
-  });
+function getVolume() {
+    return makeRequest('GET/netRemote.sys.audio.volume')
+        .then(result => parseInt(result.fsapiResponse.value[0].u8[0]));
 };
 
 //And finally export the module
-module.exports = function(ipAdress, radioPin) {
-  if (!ipAdress) throw new Error('No IP adress specified');
-  if (!radioPin) throw new Error('No PIN specified');
+module.exports = function (ipAdress, radioPin) {
+    if (!ipAdress) {
+        throw new Error('No IP adress specified');
+    }
 
-  ip = ipAdress;
-  pin = radioPin;
+    if (!radioPin) {
+        throw new Error('No PIN specified');
+    }
 
-  this.getPower = getPower;
-  this.setPower = setPower;
+    ip = ipAdress;
+    pin = radioPin;
 
-  this.getName = getName;
-  this.getText = getText;
+    this.getPower = getPower;
+    this.setPower = setPower;
 
-  this.setMute = setMute;
-  this.getMute = getMute;
+    this.getName = getName;
+    this.getText = getText;
 
-  this.setMode = setMode;
-  this.getMode = getMode;
+    this.setMute = setMute;
+    this.getMute = getMute;
 
-  this.setVolume = setVolume;
-  this.getVolume = getVolume;
+    this.setMode = setMode;
+    this.getMode = getMode;
 
+    this.setVolume = setVolume;
+    this.getVolume = getVolume;
 };
